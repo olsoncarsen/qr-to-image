70e1eee0e6e406fb12a992153f10878383347d8e
olsoncarsen <gashilovdmitry@yandex.ru>
2022-03-12 15:10:55 +0300

    working example.pdf (tested on MacOS)

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # qr-to-image
-Find qr code in pdf and return image of this qr code
-## installation
+Its a PHP CLI  tool for Mac/Linux (tested and developed in MacOS)
 
-'''
-composer install
-// If imagemagick is not installed
-brew install pkg-config imagemagick
-pecl install imagick
-'''
+## Installation
+
+1. We need to install pdfimages cli:
+
+	```sh
+	$ brew install poppler
+
+	```
+
+2. Install composer dependencies:
+
+	```sh
+	$ composer install 
+
+	```
+
+## How to use 
+
+	```sh
+	$ php index.php example.pdf
+
+	```
+After that will be generated qrcode-***.ppm images with recognized qr codes.
--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
 require __DIR__ . "/vendor/autoload.php";
 
-// $imagick = new Imagick();
-// $imagick->readImage('example.pdf[0]');
-// $imagick->writeImages('output.jpg', false);
+// extarct images from argv[1] pdf file with pdfimages cli
+$output = null;
+$retval = null;
+exec('pdfimages -j '.$argv[1].' qrcode', $output, $retval);
+echo "Returned with status $retval and output:\n";
+print_r($output);
 
-// $output = null;
-// $retval = null;
-// exec('pdfimages -j example.pdf iamge', $output, $retval)
-// echo "Returned with status $retval and output:\n";
-// print_r($output);
+// getting all files from current directory
+$output = null;
+$retval = null;
+exec('ls', $output, $retval);
 
-$qrcode = new Zxing\QrReader('image-003.ppm');
-$text = $qrcode->text();
+// looping through images from pdfimages
+foreach ($output as $imageFileName) {
+  if (substr($imageFileName, 0, 6) === 'qrcode') {
+    var_dump($imageFileName);
+    $qrcode = new Zxing\QrReader($imageFileName);
+    $text = $qrcode->text();
+    if ($text) {
+      // if we find qr code, then leave the image
+      continue;
+    } else {
+      // remove unrecognized image
+      exec('rm '.$imageFileName);
+    }
+  }
+}
 
-echo $text;
