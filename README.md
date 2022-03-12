9cd92f652e62d78ca500144c73ccf7ec2805a89f
olsoncarsen <gashilovdmitry@yandex.ru>
2022-03-12 12:18:05 +0300

    finding all images with pdfmiages cli tool

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Find qr code in pdf and return image of this qr code
 ## installation
 
+'''
 composer install
 // If imagemagick is not installed
 brew install pkg-config imagemagick
 pecl install imagick
+'''
--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 require __DIR__ . "/vendor/autoload.php";
-$imagick = new Imagick();
-$imagick->readImage('example.pdf[0]');
-$imagick->writeImages('output.jpg', false);
 
-$qrcode = new Zxing\QrReader('output.jpg');
+// $imagick = new Imagick();
+// $imagick->readImage('example.pdf[0]');
+// $imagick->writeImages('output.jpg', false);
+
+// $output = null;
+// $retval = null;
+// exec('pdfimages -j example.pdf iamge', $output, $retval)
+// echo "Returned with status $retval and output:\n";
+// print_r($output);
+
+$qrcode = new Zxing\QrReader('image-003.ppm');
 $text = $qrcode->text();
 
 echo $text;
